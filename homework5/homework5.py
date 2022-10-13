9979c09a538c23030a23ee8625cdb284af59834a
EduardTsvetkov <edvardas@inbox.ru>
2022-10-13 19:31:13 +0300

    Две задачи решены

--- homework5/homework5.py
+++ homework5/homework5.py
@@ -16,3 +16,100 @@
 # [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
 
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+
+import my_function as my
+ 
+def string_cleaning(inp_text, inp_str: str) -> str:
+    """Функция удаляет из текста слова, содержащую указанный набор символов"""
+    my_list = inp_text.split()
+    # l = []
+    # for x in my_list:
+    #     if inp_str not in x:
+    #         l.append(x)
+    # result = ' '.join(l)
+ 
+    result = ' '.join(list(filter(lambda x: inp_str not in x, my_list)))
+    return result
+ 
+def coding_word_RLE(inp_word: str) -> str:
+    """Функция возвращает закодированное RLE слово или строку"""
+    result = ''
+    saved_c = ''
+    count = 1
+ 
+    if not inp_word: 
+        return ""
+    elif len(inp_word) == 1:
+        return "1" + inp_word
+ 
+    for c in inp_word:
+        if c == saved_c:
+            count += 1
+        else:
+            if saved_c:
+                while count > 9:
+                    result += "9" + saved_c
+                    count -= 9
+                result += str(count % 9) + saved_c
+            saved_c = c    
+            count = 1
+            
+    while count > 9:
+        result += "9" + saved_c
+        count -= 9
+    result += str(count % 9) + saved_c
+ 
+    return result
+ 
+def decoding_word_RLE(inp_word: str) -> str:
+    """Функция возвращает раскодированное RLE слово или строку"""
+    result = ""
+    l = []
+    if not inp_word: 
+        return ""
+    elif len(inp_word) == 2:
+        return inp_word[1] * int(inp_word[0])
+ 
+    for i in range(1, len(inp_word), 2):
+        l.append(inp_word[i] * int(inp_word[i - 1]))
+ 
+    result = ''.join(l) 
+    return result   
+ 
+print()
+while my.make_choice("Решаем задачу 1 (удаление слов из текста),"):
+    print()
+    input_file = open("input_text.txt", "r")
+    my_text = input_file.read()
+    input_file.close()
+    
+    print("Исходный текст (из файла input_text.txt): ")
+    print(my_text)
+ 
+    outp_text = string_cleaning(my_text, "abc")
+    
+    print()
+    print("Текст без слов с 'abc' (записан в файл output_text.txt)")
+    print(outp_text)
+ 
+    output_file = open("output_text.txt", "w")
+    output_file.write(outp_text)
+    output_file.close()
+ 
+    print()
+print()
+ 
+print()
+while my.make_choice("Решаем задачу 4 (RLE)? "):
+    print()
+    inp_string = "aaadsdfbbbb  x erttyuuu   seeeeeeeeeeeeeeeeeeee z qwertttttttttt"
+    coded_string = coding_word_RLE(inp_string)
+    print("Исходная строка:")
+    print(inp_string)
+    print("Закодированая строка:")
+    print(coded_string)
+    print("Раскодированная строка:")
+    print(decoding_word_RLE(coded_string))
+    print()
+print()
