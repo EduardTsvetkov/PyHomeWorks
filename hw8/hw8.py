d554d1f958c08fc8dd51a4e08818f06381683f38
EduardTsvetkov <edvardas@inbox.ru>
2023-03-31 20:08:32 +0300

    домашка 8 - начало

---
+++ hw8/hw8.py
@@ -0,0 +1,33 @@
+# Напишите функцию read_last(lines, file), которая будет открывать определенный файл file и выводить 
+# на печать построчно последние строки в количестве lines (на всякий случай проверим, что задано 
+# положительное целое число). Протестируем функцию на файле «article.txt»
+
+# Требуется реализовать функцию longest_words(file), которая записывает в файл result.txt слово, 
+# имеющее максимальную длину (или список слов, если таковых несколько).
+
+import codecs
+
+
+def read_last(lines, file):
+    data = open(file, 'r', encoding='utf8')
+    s_list = data.readlines()
+    if len(s_list) < lines or lines < 0:
+        data.close()
+        print('Неправильное количество строк...')
+        return
+    for i in range(-lines, 0):
+        print(s_list[i])
+    data.close()
+    print('------------')
+    return
+
+
+def longest_words(file):
+    data = open(file, 'r', encoding='utf8')
+    s_list = data.readlines()
+    max_len = len(max(s_list, key=len))
+    for res in filter(lambda s: len(s) == max_len, s_list):
+        print(res)
+
+read_last(3, 'article.txt')
+longest_words('article.txt')
